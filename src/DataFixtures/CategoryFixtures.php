11a37beeecea2d488e7168c294178aefcc34759b
Styvens Wentz <styvens.wentz@gmail.com>
2024-09-02 12:23:04 +0200

    Done

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -6,24 +6,30 @@ use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+//Tout d'abord nous ajoutons la classe Factory de FakerPhp
+use Faker\Factory;
+
 class CategoryFixtures extends Fixture
 {
-    public const CATEGORIES = [
-        'Action',
-        'Aventure',
-        'Animation',
-        'Fantastique',
-        'Horreur',
-    ];
-
     public function load(ObjectManager $manager): void
     {
-        foreach (self::CATEGORIES as $key => $categoryName) {
+        //Puis ici, nous demandons à la Factory de nous fournir un Faker
+        $faker = Factory::create('fr_FR');
+
+        /**
+         * L'objet $faker que tu récupères est l'outil qui va te permettre
+         * de te générer toutes les données que tu souhaites.
+         */
+        for($i = 1; $i <= 5; $i++) {
             $category = new Category();
-            $category->setName($categoryName);
+            //Ce Faker va nous permettre d'alimenter l'instance de Category que l'on souhaite ajouter en base
+            $category->setName($faker->word());
+
             $manager->persist($category);
-            $this->addReference('category_' . $categoryName, $category);
+            // Ici, on crée une référence à l'objet $category sous la forme 'category_1', 'category_2'...
+            $this->addReference('category_' . $i, $category);
         }
+
         $manager->flush();
     }
 }
--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -7,44 +7,31 @@ use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const EPISODES = [
-        [
-            'title' => 'Passé décomposé',
-            'number' => '1',
-            'synopsis' => "Rick Grimes, shérif, est blessé à la suite d'une course-poursuite. Il se retrouve dans le 
-            coma. Cependant, lorsqu'il se réveille dans l'hôpital, il ne découvre que désolation et cadavres",
-            'season' => 'season1_Walking Dead',
-        ],
-        [
-            'title' => 'Tripes',
-            'number' => '2',
-            'synopsis' => "Rick parvient à s'échapper du tank grâce à l'aide de Glenn, dont il avait entendu la voix à 
-            la radio. Rick et Glenn se réunissent avec les compagnons de Glenn, un autre groupe de survivants dont 
-            Andrea, T-dog, Merle, Morales, Jacqui, venus pour se ravitailler au supermarché.",
-            'season' => 'season1_Walking Dead',
-        ],
-        [
-            'title' => 'T\'as qu\'à discuter avec les grenouilles',
-            'number' => '3',
-            'synopsis' => "De retour au camp avec le groupe de survivants du supermarché, Rick retrouve enfin et avec 
-            beaucoup d'émotion sa femme Lori et son fils Carl. Andrea quant à elle, rejoint sa jeune sœur Amy.",
-            'season' => 'season1_Walking Dead',
-        ],
-    ];
-
     public function load(ObjectManager $manager): void
     {
-        foreach (self::EPISODES as $key => $episodes) {
-            $episode = new Episode();
-            $episode->setTitle($episodes['title']);
-            $episode->setNumber($episodes['number']);
-            $episode->setSynopsis($episodes['synopsis']);
-            $episode->setSeason($this->getReference($episodes['season']));
-            $manager->persist($episode);
+        $faker = Factory::create('fr_FR');
+
+        // Boucle pour chaque série
+        for ($i = 1; $i <= 10; $i++) {
+            // Boucle pour chaque saison dans la série
+            for ($j = 1; $j <= 5; $j++) {
+                // Créer 10 épisodes pour chaque saison
+                for ($k = 1; $k <= 10; $k++) {
+                    $episode = new Episode();
+                    $episode->setTitle($faker->sentence(3));
+                    $episode->setNumber($k);
+                    $episode->setSynopsis($faker->paragraph());
+                    $episode->setSeason($this->getReference('season' . $i . '_' . $j));
+
+                    $manager->persist($episode);
+                }
+            }
         }
+
         $manager->flush();
     }
 
